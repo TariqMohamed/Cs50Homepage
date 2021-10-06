0ba44bf961fd36c69d3947aa15ca00ef35454121
zedgamer9128 <zeeshantechnical9128@gmail.com>
2021-10-06 12:50:30 +0530

    October 6, 2021 12:50 PM

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     function css(e, styles) {
         for (const property in styles)
-        e.style[property] = styles[property];
+            e.style[property] = styles[property];
     }
 
     let github = document.querySelector("#github");
-    css(github, {backgroundColor: "#303841", minHeight: "150px", textAlign: "center", color: "white", fontSize: "30px", fontFamily: "'Raleway', sans-serif",});
+    css(github, { backgroundColor: "#303841", minHeight: "150px", textAlign: "center", color: "white", fontSize: "30px", fontFamily: "'Raleway', sans-serif", });
 
-    let p = document.createElement("p");
-    p.textContent = "My Github";
-    css(p, {paddingTop: "20px"})
-    github.appendChild(p);
+    let h3 = document.createElement("h3");
+    h3.textContent = "My Github";
+    css(h3, { paddingTop: "20px" })
+    github.appendChild(h3);
 
     let btn = document.createElement("button");
-    btn.onclick = function(){
+    btn.onclick = function () {
         window.open("https://github.com/zedgamer9128", "_blank");
     }
-    css(btn, {display: "flex", margin: "0 auto 0 auto"})
+    css(btn, { display: "flex", margin: "0 auto 0 auto" })
     btn.style.backgroundColor = "#0b6d65";
     let btnp = document.createElement("p");
     btnp.style.margin = "-5px 0 0 5px";
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", function() {
     btnp.style.color = "white";
     btnp.textContent = "Github";
     let img = new Image();
-    img.src="GitHub-Mark-Light-32px.png";
+    img.src = "GitHub-Mark-Light-32px.png";
     img.style.margin = "1px 0 0 5px"
     btn.appendChild(img);
     btn.appendChild(btnp);
